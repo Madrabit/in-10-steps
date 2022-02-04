9af80f669b27df190d0bf07f312de7d5668b4564
madrabit <madrabot@gmail.com>
2022-02-04 11:35:21 +0300

    Inheritance - Table_per_class strategy

--- hibernate/src/main/java/ru/madrabot/hibernate/entity/Employee.java
+++ hibernate/src/main/java/ru/madrabot/hibernate/entity/Employee.java
@@ -4,8 +4,7 @@ package ru.madrabot.hibernate.entity;
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(name = "EmployeeType")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Employee {
 
     @Id
